914666b5956f80ffa3a3c0cfb2247b7fb22b504d
arcayan786-hue <arcayan786@gmail.com>
2026-05-14 00:57:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -704,3 +704,17 @@ bot.on("message", async (msg) => {
         spam[id].time = Date.now();
     }
 });
+
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Bot is running 🚀");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("🌐 Server running on port " + PORT);
+});
